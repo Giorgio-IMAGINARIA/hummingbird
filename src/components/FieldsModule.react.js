8eaf53f8112ca51de8425819b5a073b014cf44d8
Giorgio Mazzei <giorgio.mazzei@icloud.com>
2018-02-06 16:59:50 +0000

    field module done

--- src/components/FieldsModule.react.js
+++ src/components/FieldsModule.react.js
@@ -1,22 +1,59 @@
 // React
 import React, {Component} from 'react';
+// MaterialUI
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table';
 // Data
 import farm from '../data/farm.json';
 
 class FieldsModule extends Component {
   constructor(props) {
     super(props);
+    this.createFieldTable = this.createFieldTable.bind(this);
     this.state = {
-      slideIndex: 0
+      fieldsArray: []
     };
 
   }
 
+  createFieldTable() {
+    let nextArray: Array<any> = farm.fields;
+    let fieldList: Array<any> = [];
+    nextArray.forEach((item, index) => {
+      let elementToCreate: any = <TableRow key={index}>
+        <TableRowColumn>{item.name}</TableRowColumn>
+        <TableRowColumn>{item.hectares}</TableRowColumn>
+        <TableRowColumn>{item.disease_susceptibility}</TableRowColumn>
+      </TableRow>;
+      fieldList.push(elementToCreate);
+    });
+    this.setState({fieldsArray: fieldList});
+  }
+
+  componentWillMount() {
+    this.createFieldTable();
+  }
+
   render() {
     return (<div>
-      <ul>
-        {farm.fields.map(field => <li key={field.name}>{field.name}</li>)}
-      </ul>
+      <Table>
+        <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+          <TableRow>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>ha</TableHeaderColumn>
+            <TableHeaderColumn>Ds</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          {this.state.fieldsArray}
+        </TableBody>
+      </Table>
     </div>);
   }
 }
