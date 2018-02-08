b6f39d0bd38d87f90bb19070093423ebbc6ae2ba
Giorgio Mazzei <giorgio.mazzei@icloud.com>
2018-02-08 17:51:41 +0000

    first point accomplished

--- README.md
+++ README.md
@@ -14,3 +14,6 @@ The CropsModule and FieldsModule are pretty similar and show details regarding t
 # FUTURE IMPROVEMENTS
 Having had more time I would have developed a logic for selecting fields in the map and to then showing data related to that specific field in a pop-up. Also It would have been interesting to double-click in one of the rows of the tables and to have a pop-up of the map centred on that specific field which would have been highlighted with a different colour.
 That's something that I would have achieved through a Flex architecture using action-creators, dispatchers, stores and store listeners to open the pop-ups.
+
+# N.B.
+I had to use "nextFarm.centre.coordinates.reverse();" to get the right coordinates from the API. There could be a bug in the data provided.
--- src/components/MapModule.react.js
+++ src/components/MapModule.react.js
@@ -17,42 +17,26 @@ class MapModule extends Component {
     super(props);
     this.onCurrentStoreFarmChange = this.onCurrentStoreFarmChange.bind(this);
     this.state = {
-      wholeMap:'',
       mapDimensions: {
         width: '100%',
         height: '500px'
       },
-      farm:{
+      farm: {
         name: '',
-        centre:{
-          coordinates:[51.5074,0.1278],
+        centre: {
+          coordinates: [51.5074, 0.1278],
           type: "Point"
         },
-        fields:[]
+        fields: []
       }
     };
   }
 
   onCurrentStoreFarmChange() {
-  let nextFarm = StoreFarm.getFarm();
-console.log('nextFarm: ', nextFarm);
-this.setState({farm: nextFarm});
-console.log('this.state.farm.centre.coordinates: ', this.state.farm.centre.coordinates);
-
-  // let droneList: Array<any> = [];
-  // nextArray.forEach((item, index) => {
-  //   let elementToCreate: any = <TableRow key={index}>
-  //     <TableRowColumn>{item.droneId}</TableRowColumn>
-  //     <TableRowColumn>{item.name}</TableRowColumn>
-  //     <TableRowColumn>{item.numCrashes}</TableRowColumn>
-  //     <TableRowColumn>{item.numFlights}</TableRowColumn>
-  //     <TableRowColumn>{item.price}</TableRowColumn>
-  //     <TableRowColumn>{item.currency}</TableRowColumn>
-  //   </TableRow>;
-  //   droneList.push(elementToCreate);
-  // });
-  // this.setState({droneArray: droneList});
-}
+    let nextFarm = StoreFarm.getFarm();
+    nextFarm.centre.coordinates.reverse();
+    this.setState({farm: nextFarm});
+  }
 
   changeMapDimensions(previousDimensions) {
     let newHeight = window.innerHeight - 135;
@@ -66,10 +50,9 @@ console.log('this.state.farm.centre.coordinates: ', this.state.farm.centre.coord
 
   render() {
     return (<div>
-      {this.state.wholeMap}
       <Map style={this.state.mapDimensions} center={this.state.farm.centre.coordinates} zoom={13}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/> {this.state.farm.fields.map(field => <GeoJSON key={field.name} data={field.boundary}/>)}
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+        {this.state.farm.fields.map(field => <GeoJSON key={field.name} data={field.boundary}/>)}
       </Map>
     </div>);
   }
